81acb70d77fbff4a3e2e7f8c86c6cff1e550798b
Kirti <kirti.air203@gmail.com>
2024-10-30 21:34:17 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
-
-
+import React from 'react'
+import {BrowserRouter , Routes , Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import DashBoard from './pages/DashBoard'
+import Battle from './pages/Battle'
 
 function App() {
 
   return (
     <>
-      <p className='text-red-700 '>ram</p>
-        
+      <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about"
+        element={<About />} />
+        <Route path="/sign-in" 
+        element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/battle"
+        element={<Battle />} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignIn() {
+  return (
+    <>
+    SignUp
+    </>
+  )
+}
+
+export default  SignIn
