b86259c6d9b58f7c78072c5f689b7da88b751e1b
dipanshupatidar77 <dipanshupatidar77@gmail.com>
2025-08-29 18:10:51 +0530

    backend updated commit

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import e from "express";
+import fs from "fs";
+
+// dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); 
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
